c4505135bf1c237d97b0d2ba07a6c8940f4bb76c
axxx007xxxz <mattia-sky@hotmail.it>
2016-09-29 17:06:08 +0200

    anykernel.sh: Clean up

--- anykernel.sh
+++ anykernel.sh
@@ -23,7 +23,6 @@ libmodule=/system/lib/modules;
 ramdisk=/tmp/anykernel/ramdisk;
 bin=/tmp/anykernel/tools;
 split_img=/tmp/anykernel/split_img;
-patch=/tmp/anykernel/patch;
 
 chmod -R 755 $bin;
 mkdir -p $ramdisk $split_img;
@@ -107,117 +106,11 @@ write_boot() {
   dd if=/tmp/anykernel/boot-new.img of=$block;
 }
 
-# backup_file <file>
-backup_file() { cp $1 $1~; }
-
-# replace_string <file> <if search string> <original string> <replacement string>
-replace_string() {
-  if [ -z "$(grep "$2" $1)" ]; then
-      sed -i "s;${3};${4};" $1;
-  fi;
-}
-
-# replace_section <file> <begin search string> <end search string> <replacement string>
-replace_section() {
-  line=`grep -n "$2" $1 | head -n1 | cut -d: -f1`;
-  sed -i "/${2//\//\\/}/,/${3//\//\\/}/d" $1;
-  sed -i "${line}s;^;${4}\n;" $1;
-}
-
-# remove_section <file> <begin search string> <end search string>
-remove_section() {
-  sed -i "/${2//\//\\/}/,/${3//\//\\/}/d" $1;
-}
-
-# insert_line <file> <if search string> <before|after> <line match string> <inserted line>
-insert_line() {
-  if [ -z "$(grep "$2" $1)" ]; then
-    case $3 in
-      before) offset=0;;
-      after) offset=1;;
-    esac;
-    line=$((`grep -n "$4" $1 | head -n1 | cut -d: -f1` + offset));
-    sed -i "${line}s;^;${5}\n;" $1;
-  fi;
-}
-
-# replace_line <file> <line replace string> <replacement line>
-replace_line() {
-  if [ ! -z "$(grep "$2" $1)" ]; then
-    line=`grep -n "$2" $1 | head -n1 | cut -d: -f1`;
-    sed -i "${line}s;.*;${3};" $1;
-  fi;
-}
-
-# remove_line <file> <line match string>
-remove_line() {
-  if [ ! -z "$(grep "$2" $1)" ]; then
-    line=`grep -n "$2" $1 | head -n1 | cut -d: -f1`;
-    sed -i "${line}d" $1;
-  fi;
-}
-
-# prepend_file <file> <if search string> <patch file>
-prepend_file() {
-  if [ -z "$(grep "$2" $1)" ]; then
-    echo "$(cat $patch/$3 $1)" > $1;
-  fi;
-}
-
-# insert_file <file> <if search string> <before|after> <line match string> <patch file>
-insert_file() {
-  if [ -z "$(grep "$2" $1)" ]; then
-    case $3 in
-      before) offset=0;;
-      after) offset=1;;
-    esac;
-    line=$((`grep -n "$4" $1 | head -n1 | cut -d: -f1` + offset));
-    sed -i "${line}s;^;\n;" $1;
-    sed -i "$((line - 1))r $patch/$5" $1;
-  fi;
-}
-
-# append_file <file> <if search string> <patch file>
-append_file() {
-  if [ -z "$(grep "$2" $1)" ]; then
-    echo -ne "\n" >> $1;
-    cat $patch/$3 >> $1;
-    echo -ne "\n" >> $1;
-  fi;
-}
-
-# replace_file <file> <permissions> <patch file>
-replace_file() {
-  cp -pf $patch/$3 $1;
-  chmod $2 $1;
-}
-
-# patch_fstab <fstab file> <mount match name> <fs match type> <block|mount|fstype|options|flags> <original string> <replacement string>
-patch_fstab() {
-  entry=$(grep "$2" $1 | grep "$3");
-  if [ -z "$(echo "$entry" | grep "$6")" ]; then
-    case $4 in
-      block) part=$(echo "$entry" | awk '{ print $1 }');;
-      mount) part=$(echo "$entry" | awk '{ print $2 }');;
-      fstype) part=$(echo "$entry" | awk '{ print $3 }');;
-      options) part=$(echo "$entry" | awk '{ print $4 }');;
-      flags) part=$(echo "$entry" | awk '{ print $5 }');;
-    esac;
-    newentry=$(echo "$entry" | sed "s;${part};${6};");
-    sed -i "s;${entry};${newentry};" $1;
-  fi;
-}
-
 ## end methods
 
 
 ## AnyKernel install
 dump_boot;
-
-# begin ramdisk changes
-
-
-# end ramdisk changes
 
 write_boot;
 
