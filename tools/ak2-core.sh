koush/AnyKernel

commit 45bfc5d602582c53433bd2398bdc5876a7e260c9

AK2: assume sha1 if "unknown" hash type encountered - some Samsung device images can be unpacked and repacked with the standard tools but the id sha1 is offset differently than in the standard header resulting in broken hash type detection

tools/ak2-core.sh
@@ -59,6 +59,9 @@ dump_boot() {
     $bin/pxa1088-unpackbootimg -i /tmp/anykernel/boot.img -o $split_img;
   else
     $bin/unpackbootimg -i /tmp/anykernel/boot.img -o $split_img;
+    if [ "$(cat $split_img/boot.img-hash)" == "unknown" ]; then
+      echo "sha1" > $split_img/boot.img-hash;
+    fi;
   fi;
   if [ $? != 0 -o "$dumpfail" ]; then
     ui_print " "; ui_print "Dumping/splitting image failed. Aborting..."; exit 1;

axxx007xxxz/test_lux-ak2

commit 99995cb4884757a274b8a0d39b89af2ce7a00f07

Better fix for unknown hash type
- https://github.com/osm0sis/AnyKernel2/commit/45bfc5d602582c53433bd2398bdc5876a7e260c9 broke the error catching on unpackbootimg
- More easily worked around during repack, see linked commit for rationale for the assumption.

tools/ak2-core.sh
@@ -59,9 +59,6 @@ dump_boot() {
     $bin/pxa1088-unpackbootimg -i /tmp/anykernel/boot.img -o $split_img;
   else
     $bin/unpackbootimg -i /tmp/anykernel/boot.img -o $split_img;
-    if [ "$(cat $split_img/boot.img-hash)" == "unknown" ]; then
-      echo "sha1" > $split_img/boot.img-hash;
-    fi;
   fi;
   if [ $? != 0 -o "$dumpfail" ]; then
     ui_print " "; ui_print "Dumping/splitting image failed. Aborting..."; exit 1;
@@ -129,6 +126,7 @@ write_boot() {
     fi;
     if [ -f *-hash ]; then
       hash=`cat *-hash`;
+      test "$hash" == "unknown" && hash=sha1;
       hash="--hash $hash";
     fi;
     if [ -f *-unknown ]; then